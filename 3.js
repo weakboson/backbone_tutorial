2e8b29e690418c65281a79e55f6357d528f8f9f0
weakboson <weakboson@gmail.com>
2013-01-04 03:20:39 +0900

    done 3rd step

---
+++ 3.js
@@ -0,0 +1,51 @@
+(function($){
+  var Item = Backbone.Model.extend({
+    defaults: {
+      part1: 'hello',
+      part2: 'world'
+    }
+  });
+
+  var List = Backbone.Collection.extend({
+    model: Item
+  });
+
+  var ListView = Backbone.View.extend({
+    el: $('body'),
+    events: {
+      'click button#add': 'addItem'
+    },
+
+    initialize: function(){
+      _.bindAll(this, 'render', 'addItem', 'appendItem');
+      this.collection = new List();
+      this.collection.bind('add', this.appendItem); // this.collection に this.appendItem を bind するこれがキモみたいだ
+      this.counter = 0;
+      this.render();
+    },
+
+    render: function(){
+      var self = this;
+      $(this.el).append("<button id='add'>Add List item</button>");
+      $(this.el).append("<ul></ul>");
+      _(this.collection.models).each(function(item){
+        self.appendItem(item);
+      }, this);
+    },
+
+    addItem: function(){
+      this.counter++;
+      var item = new Item();
+      item.set({
+        part2: item.get('part2') + this.counter
+      });
+      this.collection.add(item); // これがトリガーになって appendItem が呼び出される↑で bind してるから
+    },
+
+    appendItem: function(item){
+      $('ul', this.el).append("<li>" + item.get('part1') + " " + item.get('part2') + "</li>");
+    }
+  });
+
+  var ListView = new ListView();
+})(jQuery);
